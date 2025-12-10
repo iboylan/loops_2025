fb27ca9fbf13de21fb0334a46469852fcdb1bee8
iboylan <pipiel326@gmail.com>
2025-12-10 19:11:34 +0000

    lelelel

--- 2_while_loops.py
+++ 2_while_loops.py
@@ -4,3 +4,8 @@ colors = ["red", "blue", "green", "yellow", "purple"]
 # Challenge:
 # Use a while loop to print each color UNTIL you find "yellow".
 # Do NOT print "yellow" — stop before it.
+
+for color in colors:
+    if color == "yellow":
+        break
+    print(color)
--- 3_looping_over_string.py
+++ 3_looping_over_string.py
@@ -4,3 +4,19 @@
 
 # Challenge:
 # Count how many vowels are in the word.
+
+word = str(input("ok: "))
+the = len(word)
+numbr = list(range(0,the+1))
+
+print(word, the)
+
+for i in numbr:
+    print(word[i])
+    i +=1
+    if i >= len(word):
+        break
+
+nv = word.find("a") + word.find("e") + word.find("i") + word.find("o") + word.find("u") + word.find("A") + word.find("I") + word.find("E") + word.find("O") + word.find("U")
+
+# print(f"# of vowels: {nv}")
